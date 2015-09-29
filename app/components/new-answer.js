4f99fd9fd278d145e3d403661b01cf3b50ac3812
Guest User <student@epicodus.com>
2015-09-29 15:01:49 -0700

    add new answer button and form

---
+++ app/components/new-answer.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true)
+    },
+
+    saveAnswer() {
+      var params = {
+        name: this.get('name'),
+        response: this.get('response'),
+        question: this.get('question')
+      };
+      this.set('addNewAnswer', false);
+      this.sendAction('saveAnswer', params);
+    }
+  }
+});
