790f026e0553c7b3118e39cf0b23db2ea6bd3aa6
Lisa Folliard <lisaf37@gmail.com>
2015-10-02 15:41:23 -0700

    add header with link

--- app/routes/index.js
+++ app/routes/index.js
@@ -10,7 +10,6 @@ export default Ember.Route.extend({
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
-    },
-    
+    }
   }
 });
